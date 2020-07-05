347f67bdfb24b07494d25f32065d583938e6f080
jelson <jelson20@outlook.com>
2020-07-05 23:56:31 +0100

    Commit

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 
 function changeImg() {
 	let img = document.querySelector('#img');
-	if (img.src.match('bulbon')) {
-		img.src = './img/pic_bulboff.gif';
+	if (img.src.match('on')) {
+		img.src = './img/pic_off.gif';
 	} else {
-		img.src = './img/pic_bulbon.gif';
+		img.src = './img/pic_on.gif';
 	}
 	console.log(img);
 }
